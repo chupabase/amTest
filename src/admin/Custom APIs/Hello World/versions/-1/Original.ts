ee084779f3649859b50efc91139e590fe7e0a12f
aryan <aryan@gmail.com>
2025-08-29 14:01:28 +0530

    custom api 1 change

--- src/admin/Custom APIs/Hello World/versions/-1/Original.ts
+++ src/admin/Custom APIs/Hello World/versions/-1/Original.ts
@@ -3,6 +3,6 @@ import * as db from 'db-interfaces';
 
 async function main(g: T.IAMGlobal) {
     // Place your code here.
-    return { hello: 'world1' };
+    return { hello: 'new response' };
 };
 module.exports = main;
